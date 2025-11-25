24271f32553df3112f18bb941bf0b62b99342ffe
daniela <danielaalgerydh@gmail.com>
2025-11-25 15:33:07 +0100

    Connect db to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.api.posts import router as posts_router
+from app.db import connect, disconnect
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await disconnect()
+
 app.include_router(posts_router, prefix="/posts", tags=["posts"])
